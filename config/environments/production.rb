15afa755a5e9247228515bd60f8747acc71b55a9
Edgar <x109justice@gmail.com>
2014-08-02 19:13:21 -0500

    return production rb to normal config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,20 +20,14 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
-  # original state 
-  # config.serve_static_assets = false
-  # modify EK 8/2/14
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  # original state 
-  # config.serve_static_assets = false
-  # modify EK 8/2/14
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
